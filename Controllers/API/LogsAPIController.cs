d4d384421b13a9c731f8506c9c94e4129cbea57a
tavilad <pintiliciucoctavian@gmail.com>
2019-06-18 18:01:06 +0300

    Initial commit

---
+++ Controllers/API/LogsAPIController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LogsTFWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace LogsTFWebApp.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsAPIController : ControllerBase
+    {
+        private const string BASE_LOGSTF_URL = "http://logs.tf/json/";
+        private JToken _token;
+        private JToken _playerToken;
+        public ActionResult GetDataForPlayerFromLog(string steamId,string logNumber)
+        {
+            PlayerViewModel data = new PlayerViewModel
+            {
+                SteamId = steamId
+            };
+
+            using(HttpClient client=new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_LOGSTF_URL);
+                //HTTP GET
+                var responseTask = client.GetAsync(logNumber);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var json = result.Content.ReadAsStringAsync();
+                    json.Wait();
+
+                    this._token = JObject.Parse(json.Result);
+
+                    this._playerToken = this._token.SelectToken("players");
+                    this._playerToken[$"[{steamId}]"].ToString();
+                    data.Kills = this._playerToken[$"[{steamId}]"]["kills"].Value<int>();
+                    data.Assists = this._playerToken[$"[{steamId}]"]["assists"].Value<int>();
+                    data.Deaths = this._playerToken[$"[{steamId}]"]["deaths"].Value<int>();
+                    data.DamagePerMinute= this._playerToken[$"[{steamId}]"]["dapm"].Value<int>();
+                    return Ok(data);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            } 
+        }
+    }
+}
---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LogsTFWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogsTFWebApp.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            PlayerViewModel data = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("LogsAPI?steamId=U:1:36224460&logNumber=2304504");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<PlayerViewModel>();
+                    readTask.Wait();
+
+                    data = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(data);
+        }
+    }
+}
