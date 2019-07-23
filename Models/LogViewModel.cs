a0547b165dfa7a930d0cb64a6ed6b0b3d0a7112f
tavilad <pintiliciucoctavian@gmail.com>
2019-07-23 17:40:58 +0300

    Added LogViewModel

---
+++ Models/LogViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogsTFWebApp.Models
+{
+    public class LogViewModel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Map { get; set; }
+        public string Date { get; set; }
+        public int Views { get; set; }
+        public int Players { get; set; }
+
+    }
+}
